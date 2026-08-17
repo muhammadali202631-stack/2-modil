d6a8f5aefa5f8fa1baf06c2f9b1a05cd0f72a7cc
muhammadali202631-stack <muhammadali202631@gmail.com>
2026-08-17 13:31:22 +0500

    sss

---
+++ scrip.js
@@ -0,0 +1,204 @@
+// const calcAverage = (score1, score2, score3) => {
+//     return (score1 + score2 + score3) / 3
+// }
+
+// const scoreDolphins = () => {
+//     return calcAverage(44, 23, 71)
+// }
+
+// const scoreKoalas = () => {
+//     return calcAverage(65, 54, 49)
+// }
+
+// const checkWinner = () => {
+//     if (scoreDolphins() > scoreKoalas()) {
+//         console.log(`Dolphins jamoasi yutdi score: ${scoreDolphins()}`)
+//     } else if (scoreDolphins() < scoreKoalas()) {
+//         console.log(`Koalas jamoasi yutdi score: ${scoreKoalas()}`)
+//     }
+// }
+
+// checkWinner()
+
+
+// 1
+// function greetUser(name) {
+//     return Salom, ${name}! Bugungi darsga xush kelibsiz!;
+// }
+
+// console.log(greetUser("Muhammadali"));
+
+
+// 2
+// function getSquare(a) {
+//     console.log(a*a); 
+// }
+// getSquare(10)
+
+
+// 3
+// function calculateAge(birthYear) {
+//    return `Hozirgi yil ${birthYear} 2026`;
+// }
+// calculateAge (Avgust)
+
+
+// 4 
+// function checkEvenOdd(a) {
+//     return a/2==0? "Juft" : "Toq"
+// }
+// checkEvenOdd(10) 
+
+// 5 
+
+// function getMax(a, b) {
+//     if (a>b) {
+//       return console.log("A soni katta + a ");
+//     }else if (a<b) {
+//         return console.log("B soni katta + b");
+//     }else if (a=b) {
+//         return console.log("Ikkala san ham teng ");
+//     }else {
+//         return console.log("Notug'ri son kiritdingiz?");   
+//     }
+//     }
+
+
+// console.log(
+//     getMax(3, 5)
+// );
+
+
+//  6 
+// function getRectangleArea (a, b) {
+//     return (a*b)
+// }
+// console.log(
+//     getRectangleArea(23, 12)
+// );
+
+
+// 7 
+// function celsiusToFahrenheit (c) {
+//     return (c*1.8+32)
+// }
+// console.log(
+//     celsiusToFahrenheit(4)
+// );
+
+
+// 8 
+// function checkGrade (a) {
+//     if (a<50) {
+//     return    console.log("Yiqildi");
+//     }else if (a>=50  && a<=100) {
+//     return    console.log("Utdi");
+//     }else{
+//     return    console.log("Bunaqa ball mavjud emas ");
+//     }
+// }
+// console.log(
+//     checkGrade(60)
+// );
+
+
+// 9 
+// function checkSign (a) {
+//     if (a/2==0) {
+//     return   console.log("Nol");
+//     }else if (a<-1) {
+//     return    console.log("Manfiy");
+//     }else if (a>0) {
+//     return    console.log("Musbat");
+//     }else{
+//     return    console.log("Xato son? ");
+//     }
+// }
+// checkSign(-4)
+
+
+
+// 10 
+// function hoursToSeconds (a) {
+//     return (a*60)
+// }
+// console.log(
+//     hoursToSeconds(12)
+// );
+
+
+
+// 11
+// function getAverage (a, b, c) {
+//     return  (a+b+c)/3
+// }
+// console.log(
+//     getAverage(12, 12, 12)
+// );
+
+
+// 12 
+// function multiplyByTen (a) {
+//     return (a*10)   
+// }
+// console.log(
+//     multiplyByTen(12)
+// );
+
+
+// 13 
+// function applyDiscount(price, discountPercent) {
+//     return price / ( price * discountPercent / 100)
+// }
+// console.log(
+//     applyDiscount(2000, 2)
+// );
+
+
+// 14 
+// function getStringLength (str) {
+//     return console.log(str . length);
+    
+// }
+//     getStringLength("Uzbekiston")
+
+
+// 15 
+// function getDayName (a) {
+//     if (a==1) {
+//         console.log("Dushanba");
+//     }else if (a==2) {
+//         console.log("Seshanba");  
+//     }else if (a==3) {
+//         console.log("Chorshanba");  
+//     }else if (a==4) {
+//         console.log("Payshanba");  
+//     }else if (a==5) {
+//         console.log("Juma");  
+//     }else if (a==6) {
+//         console.log("Shanba");  
+//     }else if (a==7) {
+//         console.log("Yakshanba");  
+//     }else{
+//         console.log("Bunaqa hafta kuni mavjud emas? ");
+//     }
+// }
+//     getDayName(4)
+
+
+// 1
+// function nameWriting(name) {
+//     return `${name.toUpperCase()} (Uzunligi: ${name.length} ta harf)`;
+// }
+// console.log(nameWriting("muhammadali")); 
+
+
+// 2 
+function name(params) {
+    
+}
+
+
+
+
+
